12d11e8df293077251487eb678b0e761af683aa8
Muhamed ibrahim <92036859+Mohammadibbu@users.noreply.github.com>
2025-07-09 22:37:38 +0530

    Update Firebase.js

--- src/DB/Firebase.js
+++ src/DB/Firebase.js
@@ -5,13 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlQkja7rqbSfM3ywOCsm_nW6iYb4rmCjo",
-  authDomain: "kealvi-badhil.firebaseapp.com",
-  databaseURL: "https://kealvi-badhil-default-rtdb.firebaseio.com",
-  projectId: "kealvi-badhil",
-  storageBucket: "kealvi-badhil.firebasestorage.app",
-  messagingSenderId: "838791733071",
-  appId: "1:838791733071:web:d3e91194e14c6385cd2f3a",
+
 };
 
 // Initialize Firebase
